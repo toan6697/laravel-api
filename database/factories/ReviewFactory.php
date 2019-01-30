1a505f14c1d1a6d7a619f65b00c7899d778921aa
toan6697 <ductoan661997@gmail.com>
2019-01-30 23:39:15 +0700

    tao du lieu fake cho product va review

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,14 @@
 <?php
 
 use Faker\Generator as Faker;
-
+use App\Model\Product;
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+    	'product_id'=>function(){
+          return Product::all()->random();
+    	},
+        'customer'=>$faker->name,
+        'review'=>$faker->word,
+        'star'=>$faker->numberBetween(0,5)
     ];
 });
